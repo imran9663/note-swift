3e07e980d653fa1f522186d331e5ccaa12642042
imran9663 <imranhussian9663@gmail.com>
2023-12-26 15:21:24 +0530

    fix(bug) : fing inline style issue

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,29 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Navbar from '../Components/Navbar';
+import TextArea from '../Components/TextArea';
 import { EditorState } from 'draft-js';
-import TextArea from '../Components/TextArea'
-import Sidebar from '../Components/Sidebar'
-import Navbar from '../Components/Navbar'
-import { MainContextProvider } from '../Context/MainContext'
-import 'draft-js/dist/Draft.css';
-import FileEditor from '../Components/FileEditor';
-
 const Layout = () => {
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
-
     return (
         <div className=' h-screen overflow-hidden bg-white dark:bg-slate-800 text-slate-900 dark:text-white'>
-            <MainContextProvider>
-                <Navbar
-                    editorState={editorState}
-                    setEditorState={setEditorState} />
-                {/* <Sidebar /> */}
-                <TextArea editorState={editorState}
-                    setEditorState={setEditorState} />
-                {/* <Dummy /> */}
-                {/* <FileEditor /> */}
-            </MainContextProvider>
+            <Navbar editorState={editorState} setEditorState={setEditorState} />
+            <TextArea editorState={editorState} setEditorState={setEditorState} />
         </div>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
---
+++ src/assets/icons/index.js
@@ -0,0 +1,23 @@
+import logo from './logo.png'
+import { ReactComponent as Bold } from './Boldsolid.svg'
+import { ReactComponent as Itaclic } from './italic-solid.svg'
+import { ReactComponent as OrderdList } from './list-ol-solid.svg'
+import { ReactComponent as UnOrderdList } from './list-ul-solid.svg'
+import { ReactComponent as Underline } from './underline-solid.svg'
+import { ReactComponent as Download } from './download.svg'
+import { ReactComponent as Plus } from './Plus.svg'
+import { ReactComponent as Upload } from './upload.svg'
+import { ReactComponent as Strike } from './Strike.svg'
+export const Icons = {
+    logo,
+    Bold,
+    Itaclic,
+    OrderdList,
+    UnOrderdList,
+    Underline,
+    Download,
+    Plus,
+    Upload,
+    Strike,
+
+}
